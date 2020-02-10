8d2b6561f60d95f4bae6a3c3f6c27be9b96ef015
Euler das Virgens <euler.virgens@envolti.com.br>
2020-02-09 21:18:34 -0300

    signUp start

---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+//Formik
+import { useFormik } from 'formik';
+//Yup
+import * as Yup from 'yup';
+//Material
+import { TextField, Typography, withStyles } from '@material-ui/core';
+
+//Styles
+import { styles } from './styles';
+
+export const SignUp = ({ classes }) => {
+	const formik = useFormik({
+		initialValues: { firstName: '', lastName: '', email: '', password: '' },
+		validationSchema: Yup.object({
+			firstName: Yup.string()
+				.max(15, 'Máximo de 15 caracteres')
+				.required('Preenchimento obrigatório'),
+			lastName: Yup.string()
+				.max(20, 'Máximo de 20 caracteres')
+				.required('Preenchimento obrigatório'),
+			email: Yup.string()
+				.email('Email inválido')
+				.required('Preenchimento obrigatório')
+		}),
+		onSubmit: values => {
+			alert(JSON.stringify(values, null, 2));
+		}
+	});
+
+	return (
+		<div className={classes.root}>
+			<form onSubmit={formik.handleSubmit} className={classes.container}>
+				<Typography>Registre-se</Typography>
+				<TextField
+					id='firstName'
+					variant='outlined'
+					label='Nome'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.firstName}
+				/>
+				<TextField
+					id='lastName'
+					variant='outlined'
+					label='Sobrenome'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.lastName}
+				/>
+				<TextField
+					id='email'
+					variant='outlined'
+					label='Email'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.email}
+				/>
+				<TextField
+					id='password'
+					type='password'
+					variant='outlined'
+					label='Senha'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.password}
+				/>
+			</form>
+		</div>
+	);
+};
+
+export default withStyles(styles)(SignUp);
---
+++ src/Components/SignUp/styles.js
@@ -0,0 +1,19 @@
+export const styles = {
+	root: {
+		display: 'flex',
+		height: '100vh',
+		width: '200vh',
+		backgroundColor: '#040303',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		marginTop: '10px',
+		backgroundColor: '#ffffff',
+		alignItems: 'center',
+		height: '453px',
+		width: '340px'
+	}
+};
