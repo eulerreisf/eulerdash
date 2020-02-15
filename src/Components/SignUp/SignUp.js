7dfb5dd33f39ec7688c0649afc36674f123b0bdb
Euler das Virgens <eulerreisf@gmail.com>
2020-02-15 14:23:04 -0300

    formik refactoring

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,78 +1,73 @@
 import React from 'react';
 
 //Formik
-import { useFormik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 //Yup
 import * as Yup from 'yup';
 //Material
-import { TextField, Typography, withStyles } from '@material-ui/core';
+import { Typography, withStyles } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
 
 //Styles
 import { styles } from './styles';
 
-export const SignUp = ({ classes }) => {
-	const formik = useFormik({
-		initialValues: { firstName: '', lastName: '', email: '', password: '' },
-		validationSchema: Yup.object({
-			firstName: Yup.string()
-				.max(15, 'Máximo de 15 caracteres')
-				.required('Preenchimento obrigatório'),
-			lastName: Yup.string()
-				.max(20, 'Máximo de 20 caracteres')
-				.required('Preenchimento obrigatório'),
-			email: Yup.string()
-				.email('Email inválido')
-				.required('Preenchimento obrigatório')
-		}),
-		onSubmit: values => {
-			alert(JSON.stringify(values, null, 2));
-		}
-	});
-
-	return (
-		<div className={classes.root}>
-			<div className={classes.container}>
-				<div className={classes.title}>
-					<Typography variant='h4'>Registre-se</Typography>
-				</div>
-				<form onSubmit={formik.handleSubmit} className={classes.form}>
-					<TextField
-						id='firstName'
-						variant='outlined'
-						label='Nome'
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.firstName}
-					/>
-					<TextField
-						id='lastName'
-						variant='outlined'
-						label='Sobrenome'
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.lastName}
-					/>
-					<TextField
-						id='email'
-						variant='outlined'
-						label='Email'
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.email}
-					/>
-					<TextField
-						id='password'
-						type='password'
-						variant='outlined'
-						label='Senha'
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.password}
-					/>
-				</form>
-			</div>
+export const SignUp = ({ classes }) => (
+	<div className={classes.root}>
+		<div className={classes.container}>
+			<Formik
+				initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+				validationSchema={Yup.object({
+					firstName: Yup.string()
+						.max(15, 'Máximo de 15 caracteres')
+						.required('Preenchimento obrigatório'),
+					lastName: Yup.string()
+						.max(20, 'Máximo de 20 caracteres')
+						.required('Preenchimento obrigatório'),
+					email: Yup.string()
+						.email('Email inválido')
+						.required('Preenchimento obrigatório')
+				})}
+				onSubmit={values => {
+					alert(JSON.stringify(values, null, 2));
+				}}
+			>
+				{({ ...props }) => (
+					<div className={classes.form}>
+						<Form>
+							<Field
+								label='Nome'
+								name='firstName'
+								type='firstName'
+								variant='outlined'
+								component={TextField}
+							/>
+							<Field
+								label='Sobrenome'
+								name='lastName'
+								type='lastName'
+								component={TextField}
+								variant='outlined'
+							/>
+							<Field
+								label='Email'
+								name='email'
+								component={TextField}
+								variant='outlined'
+							/>
+							<Field
+								label='Senha'
+								name='password'
+								type='password'
+								component={TextField}
+								variant='outlined'
+							/>
+							<button type='submit'>Submit</button>
+						</Form>
+					</div>
+				)}
+			</Formik>
 		</div>
-	);
-};
+	</div>
+);
 
 export default withStyles(styles)(SignUp);
